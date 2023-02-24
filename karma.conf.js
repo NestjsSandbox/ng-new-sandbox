4ae7920959818e585bd9418daa66b2cd600b164e
riverdev <yariv.rivercybe@gmail.com>
2023-02-24 18:11:50 +0200

    Initial commit , this version only has the result code of ng new command, nothing was added. Use this commit whenever you want to start a new practice, it's branch  will be locked as READ-ONLY so you will need to create a new branch to use it as base for your pracitce/training task

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/udm-sg-pwd'),
+      dir: require('path').join(__dirname, './coverage/ng-practice'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'udm-sg-pwd'`, () => {
+  it(`should have as title 'ng-practice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('udm-sg-pwd');
+    expect(app.title).toEqual('ng-practice');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('udm-sg-pwd app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-practice app is running!');
   });
 });
