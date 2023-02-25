abcfaf3744f473832eafc70ce8871e592e5a9b4d
Yariv G <yariv.rivercybe@gmail.com>
2023-02-25 21:18:37 +0200

    Finished the Udemy Stephen Grider Section 2 tutorial task, I installed the CSS bulma library but did not add the styling itself, so the UI looks raw.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-practice';
+  length = 0;
+  myPassword = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+
+  onChangeLength(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+
+    if (!isNaN(parsedValue)) this.length = parsedValue;
+  }
+
+  onButtonClick() {
+    this.myPassword = 'My Password';
+    console.log('Status=Letters ' + this.useLetters);
+    console.log('Status=Numbers ' + this.useNumbers);
+    console.log('Status=Symbols ' + this.useSymbols);
+    console.log('Status=Length  ' + this.length);
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
 }
