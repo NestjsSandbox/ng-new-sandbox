b02ce7cf3bd5aff09101f84b1ceb7c036f463ee6
Yariv G <yariv.rivercybe@gmail.com>
2023-02-28 18:30:11 +0200

    Added faker to generate random text and  listner & also logi to display Success only if input is equal to the random text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-practice';
+  randomText = faker.lorem.sentence().trim().substring(0, 5);
+  enteredText = '';
+
+
+
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.enteredText = target.value;
+    console.log('result = ' + this.enteredText);
+
+  }
 }
