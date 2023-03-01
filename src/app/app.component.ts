dd0bd4ab763628642a3d86e412c083da6bc5ee9c
Yariv G <yariv.rivercybe@gmail.com>
2023-03-01 15:55:37 +0200

    Finished the app, It displays 3 rows, in each you input some data then a pipe converts the input to another display format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-practice';
+ name:string = '';
+ myDate: string = '';
+ amount: number = 0;
+
+
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.name = target.value;
+  }
+
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.myDate = target.value;
+  }
+  
+  onAmountChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.amount = parseInt(target.value);
+  }
+
 }
+
